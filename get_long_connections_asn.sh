cba818be9b8a27fad5daf8a50aa02ae896923cd2
Jeff Sutch <jeff.sutch@acm.org>
2021-03-15 15:00:25 -0700

    set execute permissions on scripts. cleaned up scripts.

--- get_long_connections_asn.sh
+++ get_long_connections_asn.sh
@@ -1,5 +1,5 @@
 HOST='myhost'
 for x in `rita show-long-connections myhost| grep -v Dest| awk -F, '{print $2}'`
-> do
-> echo "$x: $(whois -h whois.cymru.com " -v $x"| grep -v BGP)";
-> done
+ do
+   echo "$x: $(whois -h whois.cymru.com " -v $x"| grep -v BGP)";
+done
